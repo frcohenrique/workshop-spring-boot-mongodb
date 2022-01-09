fc014648987b7412dff721a78a31033fb54336ea
Henrique Franco <henrique.frco@gmail.com>
2022-01-09 17:17:19 -0300

    Obtendo um usuario por id

--- src/main/java/com/henriquefranco/workshopmongo/services/UserService.java
+++ src/main/java/com/henriquefranco/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.henriquefranco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henriquefranco.workshopmongo.domain.User;
 import com.henriquefranco.workshopmongo.repository.UserRepository;
+import com.henriquefranco.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
